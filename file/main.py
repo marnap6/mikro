0f3e82cdc7098bc08b923b313c01bf7e1a0c797a
marnap6 <148319540+marnap6@users.noreply.github.com>
2024-12-29 20:46:23 +0100

    Add files via upload

---
+++ file/main.py
@@ -0,0 +1,39 @@
+from Bio import SeqIO
+
+prokka_file = "assembly.gbk"
+bakta_file = "assembly.gbff"
+
+def analyze_genes(file_path, file_format):
+    gene_lengths = []
+    polymerase_genes = []
+
+    for record in SeqIO.parse(file_path, file_format):
+        for feature in record.features:
+            if feature.type == "CDS":
+                if "translation" in feature.qualifiers:
+                    gene_length = len(feature.qualifiers["translation"][0])
+                    gene_lengths.append(gene_length)
+                if "product" in feature.qualifiers:
+                    product = feature.qualifiers["product"][0].lower()
+                    if "rna polymerase" in product or "dna polymerase" in product:
+                        polymerase_genes.append(product)
+    gene_count = len(gene_lengths)
+    avg_length = sum(gene_lengths) / gene_count if gene_count > 0 else 0
+    return gene_count, avg_length, polymerase_genes
+
+prokka_gene_count, prokka_avg_length, prokka_polymerases = analyze_genes(prokka_file, "genbank")
+bakta_gene_count, bakta_avg_length, bakta_polymerases = analyze_genes(bakta_file, "genbank")
+
+print("Porównanie wyników adnotacji:")
+print(f"Prokka - Liczba genów: {prokka_gene_count}, Średnia długość genów: {prokka_avg_length:.2f}")
+print(f"Bakta - Liczba genów: {bakta_gene_count}, Średnia długość genów: {bakta_avg_length:.2f}")
+
+print("geny kodujace polimerazy RNA i DNA w Prokka:")
+for gene in prokka_polymerases:
+    print(f"- {gene}")
+print(len(prokka_polymerases))
+
+print("geny kodujace polimerazy RNA i DNA w Bakta:")
+for gene in bakta_polymerases:
+    print(f"- {gene}")
+print(len(bakta_polymerases))
